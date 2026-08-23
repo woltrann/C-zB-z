79e59294f931f6b28cff3be3458cbbc0a12c3af5
Harun ELALTUNKARA <harunelaltunkara@gmail.com>
2026-08-23 17:16:11 +0300

    ss

--- Assets/Farland Skies/Cloudy Crown/Scripts/Info/Editor/AssetInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Borodar.FarlandSkies.CloudyCrownPro
-{
-    public class AssetInfo
-    {
-        public const string ASSET_NAME = "Farland Skies - Cloudy Crown Pro";
-        public const string ASSET_STORE_ID = "71105";
-        public const string VERSION = "1.4.2";
-    }
-}
